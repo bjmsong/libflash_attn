014e3caeafed7f0c5d46fa3a1dfddd5b41b48b2d
bjmsong <bjmsong@126.com>
2023-11-26 08:26:25 +0800

    u

---
+++ include/common.h
@@ -0,0 +1,58 @@
+// Copyright 2023. All Rights Reserved.
+// Author: Bruce-Lee-LY
+// Date: 20:42:28 on Sun, Feb 12, 2023
+//
+// Description: common macro
+
+#pragma once
+
+#include "cuda_fp16.h"
+#include "logging.h"
+#include "util.h"
+
+#if ((__CUDACC_VER_MAJOR__ == 11) && (__CUDACC_VER_MINOR__ >= 8)) || (__CUDACC_VER_MAJOR__ > 11)
+#define FAI_ENABLE_FP8
+#endif
+
+#ifdef FAI_ENABLE_FP8
+#include "cuda_fp8.h"
+#endif
+
+#define FAI_LIKELY(x) __builtin_expect(!!(x), 1)
+// 内置函数__builtin_expect提示编译器: 条件 x 的概率较小，帮助编译器优化代码
+// !!(x) 将 x 转换为布尔值
+#define FAI_UNLIKELY(x) __builtin_expect(!!(x), 0)
+
+#define FAI_CHECK(x)                      \
+    do {                                  \
+        if (FAI_UNLIKELY(!(x))) {         \
+            FLOG("Check failed: %s", #x); \
+            exit(EXIT_FAILURE);           \
+        }                                 \
+    } while (0)
+
+#define FAI_CHECK_EQ(x, y) FAI_CHECK((x) == (y))
+#define FAI_CHECK_NE(x, y) FAI_CHECK((x) != (y))
+#define FAI_CHECK_LE(x, y) FAI_CHECK((x) <= (y))
+#define FAI_CHECK_LT(x, y) FAI_CHECK((x) < (y))
+#define FAI_CHECK_GE(x, y) FAI_CHECK((x) >= (y))
+#define FAI_CHECK_GT(x, y) FAI_CHECK((x) > (y))
+
+#define FAI_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+    TypeName(const TypeName &) = delete;       \
+    void operator=(const TypeName &) = delete
+
+#define FAI_CHECK_CUDART_ERROR(_expr_)                                                            \
+    do {                                                                                          \
+        cudaError_t _ret_ = _expr_;                                                               \
+        if (FAI_UNLIKELY(_ret_ != cudaSuccess)) {                                                 \
+            const char *_err_str_ = cudaGetErrorName(_ret_);                                      \
+            int _rt_version_ = 0;                                                                 \
+            cudaRuntimeGetVersion(&_rt_version_);                                                 \
+            int _driver_version_ = 0;                                                             \
+            cudaDriverGetVersion(&_driver_version_);                                              \
+            FLOG("CUDA Runtime API error = %04d \"%s\", runtime version: %d, driver version: %d", \
+                 static_cast<int>(_ret_), _err_str_, _rt_version_, _driver_version_);             \
+            exit(EXIT_FAILURE);                                                                   \
+        }                                                                                         \
+    } while (0)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
+#include "gflags/gflags.h"
+#include "tensor.h"
 #include "flash.h"
 
-
 int main(int argc, char **argv) {
-    flash_attention_forward();
+
+    const size_t num_heads = 32;
+    const size_t dim = 128;
+    int batch = 2;
+    int seq_q = 1;
+    int seq_k = 128;
+    int seq_v = 128;
+    
+    int q_row_stride = dim;
+    int k_row_stride = dim;
+    int v_row_stride = dim;
+    int o_row_stride = dim;
+    int q_head_stride = q_row_stride * seq_q;
+    int k_head_stride = k_row_stride * seq_k;
+    int v_head_stride = v_row_stride * seq_k;
+    int o_head_stride = o_row_stride * seq_q;
+    int q_batch_stride = q_head_stride * num_heads;
+    int k_batch_stride = k_head_stride * num_heads;
+    int v_batch_stride = v_head_stride * num_heads;
+    int o_batch_stride = o_head_stride * num_heads;
+
+    float softmax_scale = 1;
+    bool is_causal = false;
+
+    size_t total_q = batch * seq_q;
+    size_t total_k = batch * seq_k;
+    size_t total_v = batch * seq_v;
+    using half = cutlass::half_t;
+
+    Tensor<half> * Q = new Tensor<half>({total_q, num_heads, dim}, "Tensor Q");
+    Tensor<half> * K = new Tensor<half>({total_k, num_heads, dim}, "Tensor K");
+    Tensor<half> * V = new Tensor<half>({total_k, num_heads, dim}, "Tensor V");
+    Tensor<half> * O = new Tensor<half>({total_q, num_heads, dim}, "Tensor O");
+
+    const half *q_ptr = Q->getHostPtr();
+    const half *k_ptr = K->getHostPtr();
+    const half *v_ptr = V->getHostPtr();
+    half *output_ptr = O->getHostPtr();
+
+    flash_attn::flash_attention_forward(q_ptr, k_ptr, v_ptr, output_ptr, batch, seq_q, seq_k, num_heads, num_heads, 
+            dim, q_batch_stride, k_batch_stride, v_batch_stride, o_batch_stride, q_head_stride, k_head_stride,
+            v_head_stride, o_head_stride, q_row_stride, k_row_stride, v_row_stride, o_row_stride,
+            softmax_scale, is_causal);
+
+    return 0;
 }
